f32a00f21c4375fb327d566e6868f569af3c6f06
socratone <gim2origin@gmail.com>
2020-11-07 23:27:20 +0900

    babel 적용

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,13 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // .js 확장자로 끝나는 파일만 babel-loader를 사용한다.
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+    ],
+  },
 };
