ebd6b185bf55a5a78c58f31712f41470106e6080
socratone <gim2origin@gmail.com>
2020-11-30 10:32:58 +0900

    index.html을 바로 쓸 수 있도록 예제 수정

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import DrawButton from './DrawButton.js';
 import './index.scss';
 
+const init = () => {
+  const screen = document.createElement('div');
+  screen.id = 'screen';
+  document.body.append(screen);
+};
+
+init();
+
 const button = new DrawButton();
 button.setScreen(document.getElementById('screen'));
 button.appendToParent(document.body);
